b04827de2d4c833a2d62fa68a9e1c4a59d7189ae
katleo rantle <kprantle@gmal.com>
2025-07-09 11:49:29 +0200

    added global theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import Providers from './Providers';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -26,11 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Providers>
           {children}
+          </Providers>
         </body>
       </html>
     </ClerkProvider>
